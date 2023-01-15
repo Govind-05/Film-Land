fa33a6788a1cc10ff3821df7a07a8514977b4410
Govind-05 <govindbhardwaj501@gmail.com>
2023-01-15 23:37:29 +0530

    add popular movies section

--- public/script/script.js
+++ public/script/script.js
@@ -1,6 +1,6 @@
 let mon=new Date().toLocaleString('default', { month: "short" });
 let day=new Date().getDate();
-
+let counter=0;
 $(".month span").text(mon);
 $(".date span").text(day);
 
@@ -11,3 +11,38 @@ $("#menu-logo-id").click(()=>{
 $("#menu-info-id").click(()=>{
     $("#menu-info-id").css("display","none");
 });
+
+$(".going-viral-container").on("mouseenter",()=>{
+    $("#prev-going-viral-card").css("opacity","0.85");
+    $("#next-going-viral-card").css("opacity","0.85");
+});
+
+$(".going-viral-container").on("mouseleave",()=>{
+    $("#prev-going-viral-card").css("opacity","0");
+    $("#next-going-viral-card").css("opacity","0");
+});
+
+function shiftNext(){
+    let translate=counter*27 + 27;
+    if(counter<2){
+        $(".going-viral-card").css("right",translate+"rem");
+        counter++;
+    }
+}
+
+function shiftPrev(){
+    let translate=counter*27 - 27;
+    if(counter>0){
+        $(".going-viral-card").css("right",+translate+"rem");
+        counter--;
+    }
+}
+
+$("#next-going-viral-card").on("click",()=>{
+    shiftNext();
+});
+
+$("#prev-going-viral-card").on("click",()=>{
+    shiftPrev();
+});
+
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+import * as dotenv from 'dotenv';
+dotenv.config()
 import express  from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
 import path from 'path';
 import { fileURLToPath } from 'url';
+import axios from "axios";
 
 const app=express();
 const __filename = fileURLToPath(import.meta.url);
@@ -12,8 +15,21 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname+"/public"));
 app.set("view engine","ejs")
 
-app.get("/",(req,res)=>{
-    res.render("home");
+app.get("/",async(req,res)=>{
+
+    const popularMoviesURL="https://api.themoviedb.org/3/movie/popular?api_key="+process.env.API_KEY+"&language=en-US&page=1";
+
+    const response=await axios.get(popularMoviesURL);
+
+    const popularMovies=[];
+    for(let i=0;i<3;i++){
+        popularMovies.push(response.data.results[i]);
+    }
+
+
+    res.render("home",{
+        popularMovies:popularMovies
+    });
 });
 
 app.listen(3000,()=>{
