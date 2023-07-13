43f6b5a1a8d4ef7aaf608688a18cc75e31097b79
Govind-05 <govindbhardwaj501@gmail.com>
2023-07-13 14:11:54 +0530

    host added

--- index.js
+++ index.js
@@ -415,6 +415,6 @@ app.post("/delete",(req,res)=>{
 
 const PORT=process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
+app.listen(PORT, "0.0.0.0",()=>{
     console.log("Server is listening on port 3000.");
 });
