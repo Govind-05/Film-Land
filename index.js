b694ed384f958e61191250961531b1f44325691b
Govind-05 <govindbhardwaj501@gmail.com>
2023-07-13 14:10:31 +0530

    port variable added

--- index.js
+++ index.js
@@ -413,6 +413,8 @@ app.post("/delete",(req,res)=>{
 
 });
 
-app.listen(3000,()=>{
+const PORT=process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
     console.log("Server is listening on port 3000.");
 });
